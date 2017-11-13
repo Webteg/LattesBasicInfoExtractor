af0e07c0aa967d817ef99464a1955dd0c35aa51d
Ricardo Terra <rterrabh@gmail.com>
2017-11-13 11:28:08 -0200

    Melhorando ainda mais a questão do cache

--- wp_lattes.php
+++ wp_lattes.php
@@ -5,33 +5,34 @@
     [lattes]
 */
 <?php
-
     function getPeople(){
-        $people = false;
-        if (function_exists('wp_cache_get')){
-            $people = wp_cache_get('vitaes');
-        }
-        if (false === $people){
-            $txt_url="http://docs.google.com/document/export?format=txt&id=12uCOqdhoLR6QO-1ed6bE3epM-UW87YVCjpq2bYrgQ6s";
-            $website = file_get_contents($txt_url);
-            $people = explode("\r\n", $website);
-            if (function_exists('wp_cache_set')){
-                wp_cache_set( 'vitaes', $people, 'default', 1); //Em produção, mudar para 600 pelo menos
-            }
-            //var_dump($people);
+        $txt_url="http://docs.google.com/document/export?format=txt&id=12uCOqdhoLR6QO-1ed6bE3epM-UW87YVCjpq2bYrgQ6s";
+        $website = file_get_contents($txt_url);
+        $people = explode("\r\n", $website);
+        if (function_exists('wp_cache_set')){
+            wp_cache_set( 'vitaes', $people, 'default', 1); //Em produção, mudar para 600 pelo menos
         }
         return $people;
     }
     
     
     function getVitaes() {
+        $vitaes = false;
+        if (function_exists('wp_cache_get')){
+            $vitaes = wp_cache_get('vitaes');
+            if (false !== $vitaes){
+                echo $vitaes;
+                return;
+            }
+        }
+        
         $foto = 'http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=';
         
         $people = getPeople();
         
-        echo '<table class="professores" border="0" cellspacing="2" cellpadding="1">';
+        $vitaes = '<table class="professores" border="0" cellspacing="2" cellpadding="1">';
         for($i = 0; $i < count($people); ++$i) {
-            echo '<tr>'."\r\n";
+            $vitaes=$vitaes.'<tr>'."\r\n";
             
             if (trim($people[$i])===''){
                 continue;
@@ -46,17 +47,22 @@
             $professor = ($data[0][0] !== ' ');
             
             if ($professor){
-                echo '<td class="foto"><div class="fotodiv" style="background-image: url('."'".$foto.$kid."'".');"></div></td>'."\r\n";
-                echo '<td class="nome" colspan="2"><a href="http://lattes.cnpq.br/'.$data[1].'">'.trim($data[0]).'</a></td>'."\r\n";
+                $vitaes=$vitaes.'<td class="foto"><div class="fotodiv" style="background-image: url('."'".$foto.$kid."'".');"></div></td>'."\r\n";
+                $vitaes=$vitaes.'<td class="nome" colspan="2"><a href="http://lattes.cnpq.br/'.$data[1].'">'.trim($data[0]).'</a></td>'."\r\n";
             }else{
-                echo '<td></td>';
-                echo '<td class="foto-aluno"><div class="foto-alunodiv" style="background-image: url('."'".$foto.$kid."'".');"></div></td>'."\r\n";
-                echo '<td class="nome-aluno"><a href="http://lattes.cnpq.br/'.$data[1].'">'.trim($data[0]).'</a> <span style="font-size: 30%">('.$data[2].')</span></td>'."\r\n";
+                $vitaes=$vitaes.'<td></td>';
+                $vitaes=$vitaes.'<td class="foto-aluno"><div class="foto-alunodiv" style="background-image: url('."'".$foto.$kid."'".');"></div></td>'."\r\n";
+                $vitaes=$vitaes.'<td class="nome-aluno"><a href="http://lattes.cnpq.br/'.$data[1].'">'.trim($data[0]).'</a> <span style="font-size: 30%">('.$data[2].')</span></td>'."\r\n";
             }
             
-            echo '</tr>'."\r\n";
+            $vitaes=$vitaes.'</tr>'."\r\n";
         }
-        echo '</table>';
+        $vitaes=$vitaes.'</table>';
+        
+        if (function_exists('wp_cache_set')){
+            wp_cache_set( 'vitaes', $vitaes, 'default', 1); //Mudar para 86400
+        }
+        echo $vitaes;
     }
 
 
